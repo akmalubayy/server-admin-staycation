e8f7cfb0af5ecb95ce976bbf70b9067edb8c126e
akmalubayy <mhmdakmal46@gmail.com>
2020-06-09 11:55:16 +0700

    Deploy Heroku

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ const flash = require('connect-flash');
 
 // Import Mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/db_staycation', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  'mongodb+srv://staycationadmin:bwamern-admin@cluster0-g96d4.mongodb.net/db_staycation?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- seed.js
+++ seed.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 // Connect to MongoDB via Mongoose
 seeder.connect(
-  'mongodb://127.0.0.1:27017/db_staycation',
+  'mongodb+srv://staycationadmin:bwamern-admin@cluster0-g96d4.mongodb.net/db_staycation?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useCreateIndex: true,
