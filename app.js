3640c31bc27ec3c1b8672af53c1088174be8847c
akmalubayy <mhmdakmal46@gmail.com>
2020-06-13 11:23:14 +0700

    Install CORS

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const cors = require('cors');
 
 // Import Mongoose
 const mongoose = require('mongoose');
@@ -46,6 +47,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   '/sb-admin-2',
